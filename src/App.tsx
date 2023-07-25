8f60da48d7df018e4f148b3b132d54a6c5ceab38
aliaypov1 <aupovamuhabat1980@gmail.com>
2023-07-25 12:31:54 +0600

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './components/style.css'
+import Card from './components/Card';
+
+const App = () => {
+ 
+  return (
+    <div>
+      <Card/>
+      dawdawdaw
+    </div>
+  );
+};
+
+export default App;
